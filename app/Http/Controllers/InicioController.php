fac1c16b753ea335e2224710938d1bd66a3da1ae
Michel Mendiola <michel.mendiola@linio.com>
2019-09-26 13:48:59 -0500

    Return something, anything :P
    Signed-off-by: Michel Mendiola <michel.mendiola@linio.com>

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -4,4 +4,8 @@ namespace App\Http\Controllers;
 
 class InicioController extends Controller
 {
+    public function __construct()
+    {
+        return view('inicio', ['something' => 'something']);
+    }
 }
